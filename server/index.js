79a41f62daef43e26d0559d855c9279143992d05
MD AWAIZ <awaizkhanmd@gmail.com>
2023-02-27 00:45:07 +0530

    commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+
+import connectDB from "./src/mongodb/connect.js";
+import userRouter from '../server/src/routes/user.routes.js';
+ import propertyRouter from "../server/src/routes/property.routes.js"
+
+
+
+ dotenv.config({path:'./src/.env'});
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", (req, res) => {
+    res.send({ message: "Hello World!" });
+});
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/properties", propertyRouter);
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+
+        app.listen(8080, () =>
+            console.log("Server started on port http://localhost:8080"),
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
